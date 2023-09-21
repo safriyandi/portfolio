bbbaea76606c91cc8516a7b43a062d332e7c578a
sfrynd <yandi@paydia.id>
2023-09-21 11:33:22 +0700

    feat: update location

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -54,14 +54,14 @@ const Header = () => {
           {/* menu end */}
 
           {/* menu-right */}
-          <div className='flex items-center gap-4'>
+          {/* <div className='flex items-center gap-4'>
             <a href={resume} download='Resume'><button className=' md:flex md:items-center mx-4 my-6 md:my-0 flex items-center gap-2 text-white font-[600] border border-solid
             border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white
             hover:font-[500] ease-in duration-200'><i class="ri-mail-download-line"></i>Resume</button></a>
             <span onClick={toggleMenu} className='text-2xl text-smallTextColor md:hidden cursor-pointer'>
               <i class="ri-menu-line"></i>
             </span>
-          </div>
+          </div> */}
         </div>
       </div>
     </header>
--- src/assets/components/UI/Contact.jsx
+++ src/assets/components/UI/Contact.jsx
@@ -22,10 +22,11 @@ const Contact = () => {
                 <h2 className='text-headingColor font-[700] text-[2.5rem] mb-8'>Get in touch</h2>
                 <div className='md:flex justify-between items-center'>
                     <div className='w-full md:w-1/2 h-300px sm:h-[450px]'>
-                        <iframe title='google-maps' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56957.53322478022!2d80.96827842824155!3d26.84485556432767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfd49fc60a843%3A0x92fc72e19b623f6a!2sGomti%20Nagar%2C%20Lucknow%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1683802476265!5m2!1sen!2sin"
+                        <iframe title='google-maps' src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=jakarta&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
                             className='border-0 w-full h-full' allowfullscreen="" loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
+
 
                     <div className='w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8 py-8'>
                         <form className='w-full' ref={form} onSubmit={sendEmail}>
